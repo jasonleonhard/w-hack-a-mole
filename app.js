862910f50b5cd058913ddefea10feb19a3ac4bf0
jasonleonhard <leonhard.jason@gmail.com>
2018-10-02 14:42:54 -0700

    increase number of cells/moles to 5x5

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ let kill_mole = (id, value) => {
   // console.log("mole:", id, 'total:', total)
 }
 
-// random number between 1 and 9
+// random number between 1 and 25
 let rand_num = () => {
-  return Math.floor(Math.random() * 9) + 1
+  return Math.floor(Math.random() * 25) + 1
 }
 
 // given some id number, toggle css class to show/not show a mole
@@ -52,15 +52,31 @@ show_rand_mole() // call on page load
 
 let check_if_mole_party = () => {
   // console.log('party')
-  if (document.getElementById(1).className == "cell mole" &&
-      document.getElementById(2).className == "cell mole" &&
-      document.getElementById(3).className == "cell mole" &&
-      document.getElementById(4).className == "cell mole" &&
-      document.getElementById(5).className == "cell mole" &&
-      document.getElementById(6).className == "cell mole" &&
-      document.getElementById(7).className == "cell mole" &&
-      document.getElementById(8).className == "cell mole" &&
-      document.getElementById(9).className == "cell mole"
+  if (document.getElementById(1).className  == "cell mole" &&
+      document.getElementById(2).className  == "cell mole" &&
+      document.getElementById(3).className  == "cell mole" &&
+      document.getElementById(4).className  == "cell mole" &&
+      document.getElementById(5).className  == "cell mole" &&
+      document.getElementById(6).className  == "cell mole" &&
+      document.getElementById(7).className  == "cell mole" &&
+      document.getElementById(8).className  == "cell mole" &&
+      document.getElementById(9).className  == "cell mole" &&
+      document.getElementById(10).className == "cell mole" &&
+      document.getElementById(11).className == "cell mole" &&
+      document.getElementById(12).className == "cell mole" &&
+      document.getElementById(13).className == "cell mole" &&
+      document.getElementById(14).className == "cell mole" &&
+      document.getElementById(15).className == "cell mole" &&
+      document.getElementById(16).className == "cell mole" &&
+      document.getElementById(17).className == "cell mole" &&
+      document.getElementById(18).className == "cell mole" &&
+      document.getElementById(19).className == "cell mole" &&
+      document.getElementById(20).className == "cell mole" &&
+      document.getElementById(21).className == "cell mole" &&
+      document.getElementById(22).className == "cell mole" &&
+      document.getElementById(23).className == "cell mole" &&
+      document.getElementById(24).className == "cell mole" &&
+      document.getElementById(25).className == "cell mole"
   ) {
     // break/quit/tally score/
     console.log('FIN')
