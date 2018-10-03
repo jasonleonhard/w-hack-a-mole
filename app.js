805cf5a3d6f67db005cfd8a34ec970fd874b8f50
jasonleonhard <leonhard.jason@gmail.com>
2018-10-03 14:24:19 -0700

    refactor: grabbing an array class and checking length simplified the logic

--- app.js
+++ app.js
@@ -64,36 +64,41 @@ let toggle_mole = (id) => {
 
 // if screen is full of moles game ends
 let check_if_mole_party = () => {
-  if (document.getElementById(1).className  == "cell mole" &&
-      document.getElementById(2).className  == "cell mole" &&
-      document.getElementById(3).className  == "cell mole" &&
-      document.getElementById(4).className  == "cell mole" &&
-      document.getElementById(5).className  == "cell mole" &&
-      document.getElementById(6).className  == "cell mole" &&
-      document.getElementById(7).className  == "cell mole" &&
-      document.getElementById(8).className  == "cell mole" &&
-      document.getElementById(9).className  == "cell mole" &&
-      document.getElementById(10).className == "cell mole" &&
-      document.getElementById(11).className == "cell mole" &&
-      document.getElementById(12).className == "cell mole" &&
-      document.getElementById(13).className == "cell mole" &&
-      document.getElementById(14).className == "cell mole" &&
-      document.getElementById(15).className == "cell mole" &&
-      document.getElementById(16).className == "cell mole" &&
-      document.getElementById(17).className == "cell mole" &&
-      document.getElementById(18).className == "cell mole" &&
-      document.getElementById(19).className == "cell mole" &&
-      document.getElementById(20).className == "cell mole" &&
-      document.getElementById(21).className == "cell mole" &&
-      document.getElementById(22).className == "cell mole" &&
-      document.getElementById(23).className == "cell mole" &&
-      document.getElementById(24).className == "cell mole" &&
-      document.getElementById(25).className == "cell mole"
-  ) {
-    // break/quit/tally score/
+  // if (
+    // document.getElementById(1).className  == "cell mole" &&
+    // document.getElementById(2).className  == "cell mole" &&
+    // document.getElementById(3).className  == "cell mole" &&
+    // document.getElementById(4).className  == "cell mole" &&
+    // document.getElementById(5).className  == "cell mole" &&
+    // document.getElementById(6).className  == "cell mole" &&
+    // document.getElementById(7).className  == "cell mole" &&
+    // document.getElementById(8).className  == "cell mole" &&
+    // document.getElementById(9).className  == "cell mole" &&
+    // document.getElementById(10).className == "cell mole" &&
+    // document.getElementById(11).className == "cell mole" &&
+    // document.getElementById(12).className == "cell mole" &&
+    // document.getElementById(13).className == "cell mole" &&
+    // document.getElementById(14).className == "cell mole" &&
+    // document.getElementById(15).className == "cell mole" &&
+    // document.getElementById(16).className == "cell mole" &&
+    // document.getElementById(17).className == "cell mole" &&
+    // document.getElementById(18).className == "cell mole" &&
+    // document.getElementById(19).className == "cell mole" &&
+    // document.getElementById(20).className == "cell mole" &&
+    // document.getElementById(21).className == "cell mole" &&
+    // document.getElementById(22).className == "cell mole" &&
+    // document.getElementById(23).className == "cell mole" &&
+    // document.getElementById(24).className == "cell mole" &&
+    // document.getElementById(25).className == "cell mole"
+  if (grab_array() == 25) { // if screen full of moles end
     console.log('FIN')
-    alert("FIN") // pause execution
+    alert("FIN") // pause execution, does not actually end yet
   }
+}
+
+// count how many moles currently exist
+let grab_array = () => {
+  return document.getElementsByClassName('cell mole').length
 }
 
 // play audio when mole clicked
